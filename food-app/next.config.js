ea656c1931ef41cc748ab951ed8deda5472d16c9
keiluck <keiluck365@gmail.com>
2025-02-08 22:37:12 +0900

    11

--- food-app/next.config.js
+++ food-app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {transpilePackages: ['antd-mobile'],}
+const nextConfig = {
+    transpilePackages: ['antd-mobile'],
+    output: "export",  // 让 Next.js 生成静态 HTML 文件
+    images: {
+      unoptimized: true,  // 解决 Amplify 不支持 Image Optimization 问题
+    },}
 
-module.exports = nextConfig
+  module.exports = nextConfig;
+  
